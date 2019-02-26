f690b7f9f84ffc9a45b28db05d678758e381f4a6
Lisa Walkosz-Migliacio <evilisa@gmail.com>
2019-02-26 11:34:55 -0600

    Don't have the application reload when connecting to a session

--- www/js/codehort.js
+++ www/js/codehort.js
@@ -1,8 +1,19 @@
 var firepad;
 var firepadRef;
+var codeMirror;
 var baseURL = window.location+"";
 var username = ""; // will be set in getPref
 var userId = Math.floor(Math.random() * 9999999999).toString(); // will be set when we authenticate
+
+//// Initialize Firebase.
+var config = {
+  apiKey: "AIzaSyCa3PLDxHQ8Fz0Gyz8-aL-Ep6WhjSgKdRc",
+  authDomain: "codehort.firebaseapp.com",
+  databaseURL: "https://codehort.firebaseio.com",
+  projectId: "codehort",
+  storageBucket: "codehort.appspot.com",
+  messagingSenderId: "323980550600"
+};
 
 //strip out anything after #
 if (baseURL != null) {
@@ -12,103 +23,82 @@ if (baseURL != null) {
 else baseURL = "";
 
 function init() {
-
   // hide panels
   hidePanels();
   getPref();
 
-    //// Initialize Firebase.
-  var config = {
-    apiKey: "AIzaSyCa3PLDxHQ8Fz0Gyz8-aL-Ep6WhjSgKdRc",
-    authDomain: "codehort.firebaseapp.com",
-    databaseURL: "https://codehort.firebaseio.com",
-    projectId: "codehort",
-    storageBucket: "codehort.appspot.com",
-    messagingSenderId: "323980550600"
-  };
   firebase.initializeApp(config);
-  //// Get Firebase Database reference.
-  firepadRef = getExampleRef();
+
+  createEditor();
+}
+
+function createEditor() {
   //// Create CodeMirror (with line numbers and the JavaScript mode).
-  var codeMirror = CodeMirror(document.getElementById('firepad-container'), {
-    lineNumbers: true,
-    mode: 'javascript'
-  });
-
-  // Get a reference to the Firebase Realtime Database
-  var chatRef = firebase.database().ref();
-  // Create an instance of Firechat
-  var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
-
-  // Listen for authentication state changes
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // If the user is logged in, set them as the Firechat user
-          console.log(user);
-          console.log("username " + username);
-          if (username == "") console.log("username is empty");
-          chat.setUser(user.uid, username);
-
-          console.log("user id: " + user.uid);
-          // if user is first, set the chatroom name to the session id and create room
-          console.log("Creating chatroom...");
-          //chat.createRoom("myRoom", "public", function(roomId) {
-
-            //console.log("Success! ID: " + roomId);
-          // if user is not first, join the chatroom name session id
-            //chat.enterRoom(roomId);
-          //});
-        //});
-    } else {
-      // If the user is not logged in, sign them in anonymously
-      firebase.auth().signInAnonymously().catch(function(error) {
-        console.log("Error signing user in anonymously:", error);
-      });
-    }
-  });
-
-  //// Create Firepad.
-  firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
-    defaultText: '// Welcome to Codehort, start coding!\n\nconsole.log(\'hello codehort!\');\n\n',
-    userId: userId
-  });
-
-  //// Create FirepadUserList (with our desired userId).
-  // problem where all of these users ends up with different name
-  var firepadUserList = FirepadUserList.fromDiv(firepadRef.child('users'),
-  document.getElementById('userlist'), userId, username);
-
-  var firepadUser = FirepadUserList.fromDiv(firepadRef.child('users'),
-  document.getElementById('user'), userId, username);
-
-  var firepadUserListDisabled = FirepadUserList.fromDiv(firepadRef.child('users'),
-  document.getElementById('userlistdisabled'), userId, username);
-
+  if (codeMirror == null) {
+    codeMirror = CodeMirror(document.getElementById('firepad-container'), {
+      lineNumbers: true,
+      mode: 'javascript'
+    });
+  }
   updateMobbing();
   changePercent(0);
 }
 
-// Helper to get hash from end of URL or generate a random one.
-function getExampleRef() {
-  var ref = firebase.database().ref();
-  var hash = window.location.hash.replace(/#/g, '');
-  hash = hash.replace(/-/g, '');
-  if (hash) {
-    ref = ref.child(hash);
-    var link = ref.key;
-    sessionId = link;
-    showSessionInfo(link);
-  } else {
-    ref = ref.push(); // generate unique location.
-    window.location = baseURL + '#' + ref.key; // add it as a hash to the URL.
-    var link = ref.key.replace(/-/g, '');
-    sessionId = link;
-    showSessionInfo(link);
+function connectFirepad() {
+  //// Get Firebase Database reference.
+  firepadRef = getFirepad();
+
+  if (firepadRef != null) {
+    // Get a reference to the Firebase Realtime Database
+    var chatRef = firebase.database().ref();
+    // Create an instance of Firechat
+    var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+
+    // Listen for authentication state changes
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // If the user is logged in, set them as the Firechat user
+        console.log(user);
+        console.log("username " + username);
+        if (username == "") console.log("username is empty");
+        chat.setUser(user.uid, username);
+
+        console.log("user id: " + user.uid);
+        // if user is first, set the chatroom name to the session id and create room
+        console.log("Creating chatroom...");
+        //chat.createRoom("myRoom", "public", function(roomId) {
+
+        //console.log("Success! ID: " + roomId);
+        // if user is not first, join the chatroom name session id
+          //chat.enterRoom(roomId);
+        //});
+      //});
+      } else {
+        // If the user is not logged in, sign them in anonymously
+        firebase.auth().signInAnonymously().catch(function(error) {
+          console.log("Error signing user in anonymously:", error);
+        });
+      }
+    });
+
+    //// Create Firepad.
+    firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
+      defaultText: '// Welcome to Codehort, start coding!\n\nconsole.log(\'hello codehort!\');\n\n',
+      userId: userId
+    });
+
+    //// Create FirepadUserList (with our desired userId).
+    // problem where all of these users ends up with different name
+    var firepadUserList = FirepadUserList.fromDiv(firepadRef.child('users'),
+    document.getElementById('userlist'), userId, username);
+
+    var firepadUser = FirepadUserList.fromDiv(firepadRef.child('users'),
+    document.getElementById('user'), userId, username);
+
+    var firepadUserListDisabled = FirepadUserList.fromDiv(firepadRef.child('users'),
+    document.getElementById('userlistdisabled'), userId, username);
+
   }
-  if (typeof console !== 'undefined') {
-    console.log('Firebase data: ', ref.toString());
-  }
-  return ref;
 }
 
 function hidePanels() {
--- www/js/session.js
+++ www/js/session.js
@@ -1,10 +1,50 @@
 var sessionId;
 
+// Helper to get hash from end of URL or generate a random one.
+function getFirepad() {
+  // grab the url hash id
+  var hash = window.location.hash.replace(/#/g, '');
+  hash = hash.replace(/-/g, '');
+  if (hash) {
+    var ref = firebase.database().ref();
+    ref = ref.child(hash);
+    var link = ref.key;
+    sessionId = link;
+    showSessionInfo(link);
+    if (typeof console !== 'undefined') {
+      console.log('Firebase data: ', ref.toString());
+    }
+  } else {
+    /*
+    ref = ref.push(); // generate unique location.
+    window.location = baseURL + '#' + ref.key; // add it as a hash to the URL.
+    var link = ref.key.replace(/-/g, '');
+    sessionId = link;
+    showSessionInfo(link);
+    */
+    console.log("do not generate a unique location at this time.");
+    // disable buttons we can't use
+    //mobbingBtn
+    //chatBtn
+    //sessionInfoBtn
+  }
+  return ref;
+}
+
 // new session icon click
 function newSession() {
   // create new session
-  window.location.href = baseURL;
-  firepadRef = getExampleRef();
+  var ref = firebase.database().ref();
+  ref = ref.push(); // generate unique location.
+  window.location = baseURL + '#' + ref.key; // add it as a hash to the URL.
+  var link = ref.key.replace(/-/g, '');
+  sessionId = link;
+  showSessionInfo(link);
+  //firepadRef = getFirepad();
+  createEditor();
+  connectFirepad();
+  closePanel();
+  //window.location.href = baseURL;
 }
 
 // join session icon click
@@ -12,7 +52,9 @@ function joinCode() {
   var sessionIdInput = document.getElementById("sessionIdInput").value;
   window.location.href = baseURL + '#-' + sessionIdInput;
   // make sure we reload the url to join correcty
-  window.location.reload(true);
+  //window.location.reload(true);
+  connectFirepad();
+  closePanel();
 }
 
 // show session info icon click
