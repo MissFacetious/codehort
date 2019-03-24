4e815dee572809ee5dce916d267c215abdab5b98
Lisa Walkosz-Migliacio <evilisa@gmail.com>
2019-03-24 16:31:18 -0500

    Give a username generator if they don't put anything

--- www/js/splash.js
+++ www/js/splash.js
@@ -5,6 +5,7 @@ function display(index) {
     document.getElementById("splashSecond").style.display = 'none';
     document.getElementById("splashThird").style.display = 'none';
     document.getElementById("splashForth").style.display = 'none';
+    nameGenerator();
   }
   if (index == 1) {
     document.getElementById("splashFirst").style.display = 'none';
@@ -41,7 +42,13 @@ function display(index) {
     splashScreen = false;
     var storage = window.localStorage;
     storage.setItem('splash', splashScreen);
-    username = document.getElementById("usernameSplashInput").value;
+
+    var usernameInput = document.getElementById("usernameSplashInput");
+    if (usernameInput.value == "") {
+      usernameInput.value = usernameInput.placeholder;
+    }
+    username = usernameInput.value;
+    document.getElementById("usernameInput").value = username;
     storage.setItem('username', username);
   }
 }
@@ -51,4 +58,21 @@ function splashChange() {
     splashScreen = false;
   else
     splashScreen = true;
+}
+
+var firstPart = ["Lightning", "Thunder", "Sky", "Sea", "Ocean", "Green", "Blue", "Red", "Yellow", "Orange", "Colorful", "Strange", "Serious", "Magnificent", "Great"]; // nouns!
+var lastPart = ["Monkey", "Squirrel", "Cat", "Mouse", "Koala", "Donkey", "Alien", "Fish", "Duck", "Goose", "Moose", "Bear", "Watermelon", "Cherry", "Strawberry", "Grape", "Blueberry", "Pineapple", "Pear", "Dog"]; // animals!
+
+function nameGenerator() {
+  var random1 = Math.round(Math.random()*firstPart.length);
+  var random2 = Math.round(Math.random()*lastPart.length);
+
+  console.log("random numbers " + random1 + " " + random2);
+  var name = firstPart[random1] + "" + lastPart[random2];
+  console.log("random name generator: " + name);
+  var usernameInput = document.getElementById("usernameSplashInput");
+  if (usernameInput.value == "") {
+
+    usernameInput.placeholder = name;
+  }
 }
