3fa2513772663607b42438cfd8fb041b0d41e14f
Lisa Walkosz-Migliacio <evilisa@gmail.com>
2019-03-26 14:23:37 -0500

    Move all light/dark attributes to codehort light and dark, codemirror is all default colors that will be overwritten

--- www/js/theme.js
+++ www/js/theme.js
@@ -22,13 +22,11 @@ function changeLight(beLight) {
 function loadTheme() {
 
   if (light) {
-    $('#css1').replaceWith('<link id="css1" rel="stylesheet" href="./css/codemirror-light.css" />');
-    $('#css2').replaceWith('<link id="css2" rel="stylesheet" href="./css/codehort-light.css" />');
+    $('#css1').replaceWith('<link id="css2" rel="stylesheet" href="./css/codehort-light.css" />');
     $('#logo').replaceWith('<img name="logo" id="logo" class="logo" src="./img/codehort-logo-white-512.png" border="0">');
   }
   else {
-    $('#css1').replaceWith('<link id="css1" rel="stylesheet" href="./css/codemirror-dark.css" />');
-    $('#css2').replaceWith('<link id="css2" rel="stylesheet" href="./css/codehort-dark.css" />');
+    $('#css1').replaceWith('<link id="css2" rel="stylesheet" href="./css/codehort-dark.css" />');
     $('#logo').replaceWith('<img name="logo" id="logo" class="logo" src="./img/codehort-logo-dark.png" border="0">');
   }
 }
