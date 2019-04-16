8306997df208746a524aa6d687f0209ec1b22cbc
Lisa Walkosz-Migliacio <evilisa@gmail.com>
2019-04-16 09:56:59 -0500

    Add config file with incorrect api key, challenges in html

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+//// Initialize Firebase.
+var config = {
+  apiKey: "#",
+  authDomain: "codehort.firebaseapp.com",
+  databaseURL: "https://codehort.firebaseio.com",
+  projectId: "codehort",
+  storageBucket: "codehort.appspot.com",
+  messagingSenderId: "#"
+};
