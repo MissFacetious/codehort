1461926ffb4d1d521763bc953184e18b3b6b9c67
Michael Migliacio <migliaci@gmail.com>
2019-04-16 20:43:27 -0500

    refactored challenge documentation

--- www/js/editor.js
+++ www/js/editor.js
@@ -93,39 +93,37 @@ var Editor = (function() {
     this.challengeNumber = challenge;
     if (challenge == '1') {
       // sample challenge
-        tempCode = "// CHALLENGE #1: addition\n// Keep this line in order to execute your code in codehort correctly.\n\n// Create a function that adds two numbers together and return the result.\nfunction plus() {\n\t\n}"
+        tempCode = "// CHALLENGE #1: addition\n// Keep this line in order to execute your code in codehort correctly.\n\n//Click the Codehort icon above to view challenge instructions and example input and output values.\nfunction plus() {\n\t\n}"
         tempTests[0] = {"test": "plus(1, 3);", "value": "4"};
         tempTests[1] = {"test": "plus(4, 5);", "value": "9"};
         tempTests[2] = {"test": "plus(-4, 5);", "value": "1"};
         tempTests[3] = {"test": "plus(10, -10);", "value": "0"};
         tempTests[4] = {"test": "plus(-10, -10);", "value": "-20"};
 
-        tempHtml = 'CHALLENGE #1: addition';
+        tempHtml = '<h2>CHALLENGE #1: addition</h2><h3>Description:</h3><p>Example challenge. Create a function that adds two parameters <i>a</i> and <i>b</i> and returns the result. </p><h3>Error Conditions:</h3><p>None</p><h3>Examples:</h3><p>plus(1, 2) will return 3</p><p>plus(5,10) will return 15</p><p>plus(-5,10) will return 5</p>';
     }
     else if (challenge == '2') {
-      tempCode = "// CHALLENGE #2: arithmetic\n// Keep this line in order to execute your code in codehort correctly.\n\n//Implement the following functions. \n" +
-      "\n //Create a function that adds two numbers together and return the result. \n function add(a, b) {\n\t\n} \n" +
-      "\n //Create a function that subtracts a from b and returns the result.\nfunction subtract(a, b) {\n\t\n}" +
-      "\n //Create a function that multiplies a and b and returns the result.\nfunction multiply(a, b) {\n\t\n}" +
-      "\n //Create a function that divides two numbers and returns the result. If divide by zero, return Infinity. \nfunction divide(a, b) {\n\t\n}" +
-      "\n //Create a function that takes the modulus of two numbers and returns the result. If incalculable, return NaN. \nfunction modulus(a, b) {\n\t\n}" +
-      "\n //Create a function that takes in a decimal number 'value' and returns that number as rounded to the number specified by 'decimal_places'\n//(for example, if value was 2.256 and decimal_places was 2, round() would return 2.26) \nfunction round(value, decimal_places) {\n\t\n}" +
-      "\n //Leave this function alone \nfunction compute(a, b) {\n\tvar val = (2 * (add(a, b) + multiply(a,b)) / (divide(a,b))) + modulus(a, b);\n\treturn round(val, 2);\n }"
+      tempCode = "// CHALLENGE #2: arithmetic\n// Keep this line in order to execute your code in codehort correctly.\n\n//Click the Codehort icon above to view challenge instructions and example input and output values.\n" +
+      "\n //Adds two numbers together and return the result. \n function add(a, b) {\n\t\n} \n" +
+      "\n //Subtracts a from b and returns the result.\nfunction subtract(a, b) {\n\t\n}" +
+      "\n //Multiplies a and b and returns the result.\nfunction multiply(a, b) {\n\t\n}" +
+      "\n //Divides two numbers and returns the result. If divide by zero, return Infinity. \nfunction divide(a, b) {\n\t\n}" +
+      "\n //Takes the modulus of two numbers and returns the result. If incalculable, return NaN. \nfunction modulus(a, b) {\n\t\n}" +
+      "\n //Takes in a decimal number 'value' and returns that number as rounded to the number specified by 'decimal_places'\n//(for example, if value was 2.256 and decimal_places was 2, round() would return 2.26) \nfunction round(value, decimal_places) {\n\t\n}" +
+      "\n //Leave this function alone. \nfunction compute(a, b, c) {\n\tvar val = (2 * (add(a, b) + multiply(a,b)) / (divide(a,b))) + modulus(a, b);\n\treturn round(val, c);\n }"
 
-      tempTests[0] = {"test": "compute(1, 2);", "value": "21"};
-      tempTests[1] = {"test": "compute(3, 4);", "value": "53.67"};
-      tempTests[2] = {"test": "compute(-1, 10);", "value": "19"};
-      tempTests[3] = {"test": "compute(10, 20);", "value": "930"};
-      tempTests[4] = {"test": "compute(423.332323423, 363.83232334);", "value": "266160.47"};
+      tempTests[0] = {"test": "compute(1, 2, 2);", "value": "21"};
+      tempTests[1] = {"test": "compute(3, 4, 2);", "value": "53.67"};
+      tempTests[2] = {"test": "compute(-1, 10, 2);", "value": "19"};
+      tempTests[3] = {"test": "compute(10, 20, 2);", "value": "930"};
+      tempTests[4] = {"test": "compute(423.332323423, 363.83232334, 2);", "value": "266160.47"};
 
-      tempHtml = 'CHALLENGE #2: arithmetic';
+      tempHtml = '<h2>CHALLENGE #2: arithmetic</h2><h3>Description:</h3><p>Create several functions to complete basic arithmetic operations. Those functions will then be used in another function, <i>compute</i>, which will return a rounded numerical result.<ul><li>Create a function <i>add(a, b)</i> that adds two parameters <i>a</i> and <i>b</i> and returns the result.</li><li>Create a function <i>subtract(a, b)</i> that subtracts parameter <i>b</i> from parameter <i>a</i> and returns the result.</li><li>Create a function <i>multiply(a, b)</i> that multiplies two parameters <i>a</i> and <i>b</i> and returns the result.</li><li>Create a function <i>divide(a, b)</i> that divides parameter <i>a</i> by parameter <i>b</i> and returns the result. If divide by zero, return Infinity.</li><li>Create a function <i>modulus(a, b)</i> that takes the modulo of two parameters <i>a</i> and <i>b</i> and returns the result. If incalculable, return NaN.</li><li>Create a function <i>round(value, decimal_places)</i> that returns the parameter <i> value </i> rounded to the value specified by <i> decimal_places </i>. Examples of target behavior shown below.</li></ul></p><h3>Error Conditions:</h3><p>None</p><h3>Examples:</h3><p>round(2.256, 2) will return 2.26</p><p>round(56.2345,3) will return 56.235</p><p>round(42,10) will return 42.</p>';
     }
     else if (challenge == '3') {
-      tempCode = "// CHALLENGE #3: roman numeral converter\n// Keep this line in order to execute your code in codehort correctly.\n\n" +
+      tempCode = "// CHALLENGE #3: roman numeral converter\n// Keep this line in order to execute your code in codehort correctly.\n\n//Click the Codehort icon above to view challenge instructions and example input and output values.\n" +
       "\n //Implement a roman numeral converter that takes a roman numeral string as input and returns its corresponding numeric value." +
-      "\n //Examples: romanNumeralToInteger(\"XXVI\") returns the integer 26; romanNumeralToInteger(\"CI\") returns the integer 101." +
-      "\n //If an invalid roman numeral or character is provided, the value -1 is returned." +
-      "\n //romanNumeralToInteger is case insensitive. You can create any helper functions you need.\nfunction romanNumeralToInteger(romanValue) {\n\t\n}";
+      "\nfunction romanNumeralToInteger(romanValue) {\n\t\n}";
 
       tempTests[0] = {"test": "romanNumeralToInteger(\'XXVI\');", "value": "26"};
       tempTests[1] = {"test": "romanNumeralToInteger(\"CI\");", "value": "101"};
@@ -133,29 +131,25 @@ var Editor = (function() {
       tempTests[3] = {"test": "romanNumeralToInteger(\"XXVIMFOO\");", "value": "-1"};
       tempTests[4] = {"test": "romanNumeralToInteger(\"MC\");", "value": "1100"};
 
-      tempHtml = '';
+      tempHtml = '<h2>CHALLENGE #3: roman numeral converter</h2><h3>Description:</h3><p>Implement a roman numeral converter function, <i>romanNumeralToInteger(romanValue)</i> that takes a string in a roman numeral format, <i>romanValue</i> as input and returns its corresponding numeric value. An explanation of roman numerals and their values can be found <a href=\'http://mathworld.wolfram.com/RomanNumerals.html\'>here</a>.</p><h3>Error Conditions:</h3><p>Roman numerals provided to the <i>romanNumeralToInteger</i> function can be either uppercase or lowercase. If an invalid roman numeral string (with non-roman-numeral characters, empty, or null) is provided, the function should return -1.</p><h3>Examples:</h3><p>romanNumeralToInteger("viii") will return 8</p><p>romanNumeralToInteger("IX") will return 9</p><p>romanNumeralToInteger("xli") will return 41</p><p>romanNumeralToInteger("LII") will return 52</p><p>romanNumeralToInteger("foo") will return -1</p>';
     }
     else if (challenge == '4') {
-      tempCode = "// CHALLENGE #4: JSON Parsing\n// Keep this line in order to execute your code in codehort correctly.\n\n" +
+      tempCode = "// CHALLENGE #4: JSON Parsing\n// Keep this line in order to execute your code in codehort correctly.\n\n//Click the Codehort icon above to view challenge instructions and example input and output values.\n" +
       "\n//Given the student list shown in the function below, implement the listed functions to compute and retrieve student information.\n"+
-      "\n//Access the database in your implementations using the function shown here:\n"+
+      "\n//Access the student records in your implementations using the function shown here:\n"+
       "\n function getStudentList() { var studentRecords = [\n\t{\n\t\"studentName\": \"Miggs\",\n\t\"studentId\": \"12345\",\n\t\"grades\": [{\n\t\t\"assignmentName\": \"project\",\n\t\t\"score\": 88,\n\t\t\"weight\": 0.2\n\t},\n\t{\n\t\t\"assignmentName\": \"test1\",\n\t\t\"score\": 82,\n\t\t\"weight\": 0.4\n\t},\n\t{\n\t\t\"assignmentName\": \"test2\",\n\t\t\"score\": 94,\n\t\t\"weight\": 0.4\n\t}\n\t]\n\t},"+
       "\n\t{\n\t\"studentName\": \"Lisa\",\n\t\"studentId\": \"67890\",\n\t\"grades\": [{\n\t\t\"assignmentName\": \"project\",\n\t\t\"score\": 94,\n\t\t\"weight\": 0.2\n\t},\n\t{\n\t\t\"assignmentName\": \"test1\",\n\t\t\"score\": 90,\n\t\t\"weight\": 0.4\n\t},\n\t{\n\t\t\"assignmentName\": \"test2\",\n\t\t\"score\": 92,\n\t\t\"weight\": 0.4\n\t}\n\t]\n\t}," +
       "\n\t{\n\t\"studentName\": \"Slacky\",\n\t\"studentId\": \"64732\",\n\t\"grades\": [{\n\t\t\"assignmentName\": \"project\",\n\t\t\"score\": 34,\n\t\t\"weight\": 0.2\n\t},\n\t{\n\t\t\"assignmentName\": \"test1\",\n\t\t\"score\": 68,\n\t\t\"weight\": 0.4},\n\t{\n\t\t\"assignmentName\": \"test2\",\n\t\t\"score\": 86,\n\t\t\"weight\": 0.4\n\t}\n\t]\n\t}\n]\n" +
       "\n return studentRecords; } \n"+
 
       "\n\n //Implement a function that returns the weighted numerical grade for a student given their name (shown in JSON as studentName)." +
-      "\n //Weighted numerical grade is determined by multiplying each grade entry by its weighted value and then summing the results (see: https://www.rapidtables.com/calc/grade/grade-calculator.html)." +
-      "\n //Hint: access the student list using the provided function." +
-      "\n //If the student is not found in the database, return -1.\nfunction getWeightedNumericalGrade(studentName) {\n\t\n}" +
+      "\nfunction getWeightedNumericalGrade(studentName) {\n\t\n}" +
 
       "\n\n //Implement a function that returns the letter grade for a student given their name (shown in JSON as studentName)." +
-      "\n //For the sake of this exercise, A = 90+, B=80-89, C=70-79, D=65-69, F=64 and below." +
-      "\n //Hint: you may want to utilize the function written above to retrieve the weighted numerical grade in your implementation." +
-      "\n //If the student is not found in the database, return -1.\nfunction getLetterGrade(studentName) {\n\t\n}" +
+      "\nfunction getLetterGrade(studentName) {\n\t\n}" +
 
       "\n\n //Implement a function that returns the score for an assignment given the student name and assignment name." +
-      "\n //If the student or assignment name is not found in the database, return -1.\nfunction getScoreForAssignment(studentName, assignmentName) {\n\t\n}"
+      "\nfunction getScoreForAssignment(studentName, assignmentName) {\n\t\n}"
 
       tempTests[0] = {"test": "getWeightedNumericalGrade(\'Miggs\');", "value": "88"};
       tempTests[1] = {"test": "getWeightedNumericalGrade(\'Ash\');", "value": "-1"};
@@ -164,7 +158,7 @@ var Editor = (function() {
       tempTests[4] = {"test": "getScoreForAssignment(\'Miggs\',\'project'\);", "value": "88"};
       //tempTests[5] = {"test": "getScoreForAssignment(\'Miggs\',\'fakeExam'\);", "value": "-1"};
       //tempTests[6] = {"test": "getScoreForAssignment(\'Ash\',\'project'\);", "value": "-1"};
-      tempHtml = '';
+      tempHtml = '<h2>CHALLENGE #4: JSON Parsing</h2><h3>Description:</h3>Given the student record information given in the <i>getStudentList</i> function provided in the challenge, implement functions to perform the operations listed below. <ul><li>Implement a function <i>getWeightedNumericalGrade(studentName)</i> that returns the weighted numerical grade for a student given the value provided in <i>studentName</i>. You can access all student information, including student name and assignment grades, using the <i>getStudentList</i> function provided for you. Hint: A weighted numerical grade is determined by multiplying each grade entry by its weighted value and then summing the results, see explanation <a href=\'https://www.rapidtables.com/calc/grade/grade-calculator.html\'>here</a>.</li><li>Implement a function <i>getLetterGrade(studentName)</i> that returns the letter grade as a string for a student given the value provided in <i>studentName</i>. For the sake of this exercise, A = 90+, B=80-89, C=70-79, D=65-69, F=64 and below. Hint: You may want to utilize the function written above to retrieve the weighted numerical grade in your implementation.</li><li>Implement a function <i>getScoreForAssigment(studentName, assignmentName)</i> that returns the score for an assignment given the values provided in <i>studentName</i> and <i>assignmentName</i>.</li></ul></p><h3>Error Conditions:</h3><p>If <i>studentName</i> or <i>assignmentName</i> information is not found in the student list, any function accessing that information should return -1 (since the results cannot be accurately computed.)</p><h3>Examples:</h3><p>getWeightedNumericalGrade(\'Miggs\') will return 88</p><p>getLetterGrade(\'Slacky\') will return D</p><p>getScoreForAssignment(\'Lisa\', \'project\') will return 94</p><p>getWeightedNumericalGrade(\'Ash\') will return -1</p>';
     }
   }
   var blob;
